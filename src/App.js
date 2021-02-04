eb5f6dd2287972edf927fecdca2b32e7fb3ef57e
Robert Ludwig <robert@ludwig-jahn.com>
2021-02-04 16:15:14 +0100

    Add very simple import funktion for csv

--- src/App.js
+++ src/App.js
@@ -1,13 +1,48 @@
 import './style/App.css';
-import Workspace from "./components/Workspace";
+import Workspace from './components/Workspace';
+import React from "react";
 
 
-function App() {
-  return (
-    <div className="App">
-      <Workspace/>
-    </div>
-  );
+class App extends React.Component{
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      notes: ['A', 'List', 'of', 'words']
+    };
+
+    this.fileInput = React.createRef();
+
+    this.showFile = this.showFile.bind(this);
+  }
+
+  handleData = (e) => {
+    const data = e.target.result.split('\n');
+    console.log(data);
+    this.setState({notes: data});
+  }
+
+  showFile() {
+    let file = this.fileInput.current.files[0];
+    let reader = new FileReader();
+
+    reader.onloadend = this.handleData;
+
+    reader.readAsText(file);
+  }
+
+
+
+  render() {
+    return (
+      <div className="App">
+        <div className='topBar'>
+          <input type='file' ref={this.fileInput} onChange={this.showFile} />
+        </div>
+        <Workspace notes={this.state.notes}/>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Workspace.js
+++ src/components/Workspace.js
@@ -2,16 +2,15 @@ import React from "react";
 import '../style/App.css'
 import Note from "./Note";
 
-import {TransformWrapper, TransformComponent} from "react-zoom-pan-pinch";
+export default function Workspace(props) {
 
-export default function Workspace() {
+  const notesList = props.notes.map((text) =>
+    <Note key={text} text = {text} />
+  );
+
   return (
     <div id='Workspace'>
-      <Note text='Das'/>
-      <Note text='ist'/>
-      <Note text='ein'/>
-      <Note text='test:'/>
-      <Note text='test!'/>
+      {notesList}
     </div>
   );
 }
